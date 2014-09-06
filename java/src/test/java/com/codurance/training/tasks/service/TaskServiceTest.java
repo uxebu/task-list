a8e80d88f489fa9155b4095dca3b147a68f1a8b8
Urs Metz <urs.metz@andrena.de>
2014-09-06 12:56:29 +0200

    tests for adding and retrieving tasks for projects

--- java/src/test/java/com/codurance/training/tasks/service/TaskServiceTest.java
+++ java/src/test/java/com/codurance/training/tasks/service/TaskServiceTest.java
@@ -1,13 +1,14 @@
 package com.codurance.training.tasks.service;
 
+import com.codurance.training.tasks.Task;
+import org.hamcrest.FeatureMatcher;
+import org.hamcrest.Matcher;
 import org.junit.Test;
 
 import java.util.Collection;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.empty;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 public class TaskServiceTest {
 
@@ -26,5 +27,46 @@ public class TaskServiceTest {
 
         Collection<String> allProjects = taskService.findAllProjects();
         assertThat(allProjects, contains("first project", "second project"));
+    }
+
+    @Test
+    public void tryingToAddTaskForNonExistingProjectResultsInAnError() throws Exception {
+        ActionResult actionResult = taskService.addTaskToProject("project", "a task");
+
+        assertThat(actionResult.failed(), is(true));
+    }
+
+    @Test
+    public void addingTaskToExistingProjectDoesNotResultInAnError() throws Exception {
+        taskService.addProject("project");
+        ActionResult actionResult = taskService.addTaskToProject("project", "a task");
+
+        assertThat(actionResult.failed(), is(false));
+    }
+
+    @Test
+    public void askingForTaskOfNonExistingProjectResultsInNull() throws Exception {
+        Collection<Task> tasksForProject = taskService.findAllTasksForProject("project does not exist");
+
+        assertThat(tasksForProject, is(nullValue()));
+    }
+
+    @Test
+    public void retrievingTaskAddedToProject() throws Exception {
+        taskService.addProject("project");
+        taskService.addTaskToProject("project", "a task");
+
+        Collection<Task> tasks = taskService.findAllTasksForProject("project");
+
+        assertThat(tasks, contains(taskWithDescription("a task")));
+    }
+
+    private Matcher<Task> taskWithDescription(String description) {
+        return new FeatureMatcher<Task, String>(equalTo(description), "task with description", "description") {
+            @Override
+            protected String featureValueOf(Task task) {
+                return task.getDescription();
+            }
+        };
     }
 }
